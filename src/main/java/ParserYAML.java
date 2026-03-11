5227b687cf75145a53c0a715ece2c7ce45eb59bd
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-11 23:11:22 +0300

    Создание YAML парсера

---
+++ src/main/java/ParserYAML.java
@@ -0,0 +1,33 @@
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ParserYAML {
+    private final Yaml yaml;
+
+    public ParserYAML() {
+        LoaderOptions options = new LoaderOptions();
+        yaml = new Yaml(options);
+    }
+
+    public List<Participiant> parse(String filePath) {
+        try (InputStream inputStream = new FileInputStream(filePath)) {
+            List<Map<String, Object>> listMap = yaml.load(inputStream);
+            List<Participiant> result = new ArrayList<>();
+
+            for (Map<String, Object> map : listMap) {
+                String name  = (String) map.get("name");
+                String style = (String) map.get("style");
+                result.add(new Participiant(name, style));
+            }
+            return result;
+        } catch (IOException e) {
+            System.err.println("Файл не найден!");
+            return new ArrayList<>();
+        }
+    }
+}
