70b26b806aaadadd2e4282aa3d2d047451e16a19
Tovarish Blinov <zhikharevsv05@gmail.com>
2026-03-11 23:11:55 +0300

    Получение участников и начало карнавала

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Carnaval carnaval = new Carnaval();
+        ParserYAML parser = new ParserYAML();
+        List<Participiant> participiants = parser.parse("C:/Users/HONOR/Documents/Теория и технология программирования/Домашняя работа 1/participiants.yaml");
+        for(Participiant participiant : participiants) {
+            carnaval.addParticipiant(participiant);
+        }
+        carnaval.dance();
     }
 }
